588d365479d2852919676be4f39d765c0e835923
Mikuri-1999 <hello.saksit1999@gmail.com>
2024-05-13 21:06:32 +0700

    Delete Conn

--- Class/Conn.cs
+++ Class/Conn.cs
@@ -8,9 +8,9 @@ namespace POS_MoonDust.Class
 {
     public class Conn
     {
-        public const string connectionString = "Host=localhost;Database=Moondust;Username=postgres;Password=022477";
+        public const string connectionString = "";
         public const string DropboxAccessToken = "";
-        public const string ClientImgur = "24ac7f66f19666d";
-        public const string ClientSecretImgur = "d15e4fbf60a087069172fbf58863c89e1484ce0e";
+        public const string ClientImgur = "";
+        public const string ClientSecretImgur = "";
     }
  }
